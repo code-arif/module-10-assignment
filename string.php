c7bcfebcca8ddfb084ebbe941c2022b267f2faa8
Md. Ariful Islam <123765578+code-arif@users.noreply.github.com>
2024-08-25 23:26:36 +0600

    problem-01: Vewel count, Reverse string, Print original string

--- string.php
+++ string.php
@@ -1,6 +1,6 @@
 <?php
 function countVowels($str) {
-    $vowels = ['a', 'e', 'i', 'o', 'u'];
+    $vowels = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'];
     $count = 0;
     for ($i = 0; $i < strlen($str); $i++) {
         if (in_array(strtolower($str[$i]), $vowels)) {
